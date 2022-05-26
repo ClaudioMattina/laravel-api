d7219e0e1e0628b79cfd94ed8a25fc0d0f7dffda
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-05-26 17:34:19 +0200

    rotta any e creazione della cartella guest con la corrispettiva home

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->
     Route::resource('posts','PostController');
 });
 
+/* qualunque rotta sbagliata porterà a questa view */
 Route::get("{any?}", function(){
     return view('guest.home');
 })->where("any",".*");
